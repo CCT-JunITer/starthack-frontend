7abe9750a9c911989129fd284187b6a7f2196604
Tomijange <tomikeyke@gmail.com>
2021-03-20 18:52:03 +0100

    feat: navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,81 @@
 <template>
   <v-app>
+    <v-navigation-drawer v-model="sidebar" app>
+      <v-list dense nav>
+        <v-list-item
+          color="red"
+          v-for="(item, i) in menuItems"
+          exact
+          :key="i"
+          :to="item.path">
+          <v-list-item-icon>
+            <v-icon>
+              {{ item.icon }}
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            {{item.title}}
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-app-bar
+      fixed
       app
-      color="primary"
+      color="#e00025"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+      <v-app-bar-nav-icon class="hidden-md-and-up" @click="sidebar = !sidebar"></v-app-bar-nav-icon>
+      <v-toolbar-title>St. Gallen</v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text v-for="item in menuItems" :key="item.title" :to="item.path">
+          <v-icon>
+            {{ item.icon }}
+          </v-icon>
+          {{item.title}}
+        </v-btn>
+      </v-toolbar-items>
+      <v-spacer></v-spacer>
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink"
+        contain
+        src="@/assets/wappen.svg"
+        transition="scale-transition"
+        width="27"
+      />
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
-    <v-main>
+    <v-main style="z-index: 0">
       <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import { Vue, Component, Prop } from 'vue-property-decorator';
 
-export default Vue.extend({
-  name: 'App',
+@Component({})
+export default class App extends Vue {
+  menuItems = [
+    {
+      path: '/',
+      title: 'Map',
+      icon: 'mdi-map-marker',
+    },
+    {
+      path: '/vote',
+      title: 'Vote',
+      icon: 'mdi-crane',
+    },
+  ];
 
-  components: {
-  },
-
-  data: () => ({
-    //
-  }),
-});
+  sidebar = false;
+}
 </script>
+
+<style lang="scss">
+
+</style>
